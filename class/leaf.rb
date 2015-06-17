03206eb9ea7cf956825c6a93f49655d224b8f0e6
bamzoni <bamzoni9@gmail.com>
2015-06-17 20:58:21 +0200

    now cuts off too long values

--- class/leaf.rb
+++ class/leaf.rb
@@ -1,13 +1,15 @@
 require_relative 'graph'
 
 class Leaf
-  attr_accessor :graph, :path, :preceeding_leaf, :alive, :child_leaves
-  def initialize(new_graph, parent_leaf, path)
+  attr_accessor :graph, :cost, :path, :preceeding_leaf, :alive, :child_leaves, :max_length
+  def initialize(new_graph, parent_leaf, path, max_length, cost)
     self.graph = new_graph
     self.path = path
     self.alive = 1
+    self.max_length = max_length
     self.preceeding_leaf = parent_leaf
-    print_path
+    self.cost = cost
+    update_info
   end
 
   def sprout_leaves
@@ -15,7 +17,8 @@ class Leaf
     self.path[-1].arcs.each do | arc | #TODO metoda zwracająca ostatni node w scieżce
       new_path = self.path.dup
       new_path.push(self.graph.nodes[arc.successor.code])
-      tmp_leaf = Leaf.new(self.graph, self, new_path)
+      tmp_cost = self.cost + arc.weight
+      tmp_leaf = Leaf.new(self.graph, self, new_path, self.max_length, tmp_cost)
       self.child_leaves.push(tmp_leaf)
     end
     self.alive = 0
@@ -24,7 +27,7 @@ class Leaf
   def initialize_sprouting
     if self.alive==1
       self.sprout_leaves
-    else
+    elsif self.child_leaves !=nil
       self.child_leaves.each do | leaf |
         leaf.initialize_sprouting
       end
@@ -39,8 +42,31 @@ class Leaf
   end
 
   def update_info
-    self.nucleotides_count = self.path.count
+    self.path.count
+    f = (self.path.count * path[0].code.length) - self.cost
+    if f==max_length
+      print f
+      print ' '
+      print_path
+    elsif f>max_length
+      self.alive=0
+    end
   end
 
+  def check_alive
+    if self.alive==1
+      return 1
+      puts 'return 1'
+    elsif self.child_leaves!=nil
+      self.child_leaves.each do | child |
+        if child.check_alive==1
+          return 1
+        end
+    end
+    else
+    # puts 'return 0'
+    return 0
+    end
+  end
 
 end
--- class/tree.rb
+++ class/tree.rb
@@ -3,13 +3,18 @@ require_relative 'graph'
 
 class Tree
   attr_accessor :root
-  
-  def initialize(graph, path)
-    self.root = Leaf.new(graph, nil, path)
+
+  def initialize(graph, path, constraint)
+    self.root = Leaf.new(graph, nil, path, constraint, 0)
   end
 
   def run
+    flag = 1
+    while flag ==1 do
+    flag = self.root.check_alive
     self.root.initialize_sprouting
+    end
+    puts 'method ended'
   end
 
 end
